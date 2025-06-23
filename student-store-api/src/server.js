dabc3c0aa8d3757fc18fe609314044dca2fa9508
Heiryn Hernandez Rojas <heiryn13@gmail.com>
2025-06-23 09:48:25 -0700

    milestone 0-1 - setting up project, and product model

--- student-store-api/src/server.js
+++ student-store-api/src/server.js
@@ -1,1 +1,26 @@
+const express = require('express');
+// const morgan=require("morgan");
+const app = express();
+;
+app.use(express.json());
+app.use("/products", productsRouter);
+// app.use(morgan("dev"));
 
+
+const PORT =3000;
+const productsRouter = require("../routes/productRoutes");
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my Student Store!');
+})
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
